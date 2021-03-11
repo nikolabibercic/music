6abcd4dced5823da587b5146f2503fe6c2ad3677
nikolabibercic <nikolabibercic@gmail.com>
2021-03-11 18:31:38 +0100

    how it works stranica

---
+++ how-it-works.php
@@ -0,0 +1,25 @@
+<?php include "partials/header.php"; ?>
+
+    <!--hivHeader-->
+    <section class="hivHeader">
+        <h2 class="container">How it works</h2>
+        <article class="container">
+            <div>
+                <i class="fas fa-user-circle"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit accusantium harum adipisci molestias corrupti repellendus praesentium enim, facilis quia architecto!</p>
+            </div>
+            <div>
+                <i class="fas fa-drum"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit accusantium harum adipisci molestias corrupti repellendus praesentium enim, facilis quia architecto!</p>
+            </div>
+            <div>
+                <i class="fas fa-cloud-download-alt"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit accusantium harum adipisci molestias corrupti repellendus praesentium enim, facilis quia architecto!</p>
+            </div>
+        </article>
+    </section>
+
+<?php include "partials/footer.php"; ?>
