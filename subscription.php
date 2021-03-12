0cb60e6ff07bd6055ef577ddae126cbabe38669b
nikolabibercic <nikolabibercic@gmail.com>
2021-03-12 18:19:45 +0100

    gotov sajt music

--- subscription.php
+++ subscription.php
@@ -1,4 +1,25 @@
 <?php include "partials/header.php"; ?>
+
+    <section class="subscription">
+        <article class="container">
+            <div>
+                <h2>Subscription from $15/month</h2>
+                <h4>Start a free trial now</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi soluta corporis inventore nesciunt minus vel itaque dignissimos tempore eveniet accusamus harum excepturi sit rem, totam porro placeat aspernatur ea quisquam.</p>
+                <button>Start free trial now</button>
+            </div>
+            <div>
+                <ul>
+                    <li>Play any track</li>
+                    <li>Listen offline</li>
+                    <li>No add interruptions</li>
+                    <li>Unlimited skips</li>
+                    <li>High quality</li>
+                    <li>Shuffle play</li>
+                </ul>
+            </div>
+        </article>
+    </section>
     <!--artists-->
     <section class="container concept">
         <article>
